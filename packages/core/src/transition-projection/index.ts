2591edcfca2efb1f5d2c14ba5a2581f2e8aeecab
etht3x <etht3x@gmail.com>
2026-07-29 16:21:42 +1000

    feat(core): add Transition product projection (#1235)

---
+++ packages/core/src/transition-projection/index.ts
@@ -0,0 +1,21 @@
+export { projectTransitionView } from './project';
+export {
+  type ActionCapabilityView,
+  type ClaimOrigin,
+  type ClaimView,
+  type LegacyTransitionViewV1,
+  type ProjectionCapabilityContext,
+  type ProjectionCapabilityReason,
+  type ProjectionCapabilityReasonCode,
+  type ProjectLegacyTransitionInput,
+  type ProjectTransitionGraphInput,
+  type ProjectTransitionViewInput,
+  TRANSITION_VIEW_SCHEMA,
+  type TransitionCapabilitiesView,
+  type TransitionChecksView,
+  type TransitionDecisionView,
+  type TransitionGraphViewV1,
+  type TransitionHistoryView,
+  type TransitionStatementAuditView,
+  type TransitionViewV1,
+} from './types';
